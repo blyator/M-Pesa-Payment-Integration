ef0d635a771696ec4783c5ec3231d9b8e124b1e0
Billy Yator <dmnbilly@gmail.com>
2025-12-11 15:00:04 +0300

    update db

--- backend/mpesa/settings.py
+++ backend/mpesa/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,14 +86,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mpesa.wsgi.application'
 
 
+import dj_database_url
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -133,10 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 # M-Pesa Configuration
 MPESA_ENVIRONMENT = os.getenv("MPESA_ENVIRONMENT", "sandbox")
